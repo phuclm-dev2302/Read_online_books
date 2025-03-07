d929879730279184a49f9018c659d5ef6617ed3e
Phuc <lephuc04b3@gmail.com>
2025-03-07 22:07:45 +0700

    feat: add book entity

---
+++ src/main/java/com/example/read_book_online/entity/Book.java
@@ -0,0 +1,32 @@
+package com.example.read_book_online.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookId;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
+
+    private String title;
+
+    private String pdfFilePath;
+
+    private Long views; // Tổng lượt xem
+
+    private Long likes;
+}

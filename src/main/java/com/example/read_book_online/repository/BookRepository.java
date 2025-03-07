b2a5148d37f7993c9f394189c63bb1a88530078f
Phuc <lephuc04b3@gmail.com>
2025-03-07 22:08:01 +0700

    feat: add BookRepository

---
+++ src/main/java/com/example/read_book_online/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.example.read_book_online.repository;
+
+import com.example.read_book_online.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}

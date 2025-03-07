4c4d132b63f6f47e7caff3d15c21e697f644e3ca
Phuc <lephuc04b3@gmail.com>
2025-03-07 22:08:08 +0700

    feat: add BookService

---
+++ src/main/java/com/example/read_book_online/service/BookService.java
@@ -0,0 +1,10 @@
+package com.example.read_book_online.service;
+
+import com.example.read_book_online.dto.request.BookRequest;
+import com.example.read_book_online.dto.response.BookResponse;
+import com.example.read_book_online.dto.response.ResponseData;
+
+public interface BookService {
+    ResponseData<BookResponse> addBook(BookRequest bookRequest);
+    ResponseData<BookResponse> getBookById(Long bookId);
+}

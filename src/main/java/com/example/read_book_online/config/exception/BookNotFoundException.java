47052509de7e322226dad7c9cb42b8eb1165a176
Phuc <lephuc04b3@gmail.com>
2025-03-07 22:11:18 +0700

    feat: add BookNotFoundException

---
+++ src/main/java/com/example/read_book_online/config/exception/BookNotFoundException.java
@@ -0,0 +1,5 @@
+package com.example.read_book_online.config.exception;
+
+public class BookNotFoundException extends RuntimeException {
+    public BookNotFoundException(String message) {super(message);}
+}

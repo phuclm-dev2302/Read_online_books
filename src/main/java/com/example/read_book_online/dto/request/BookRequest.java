c8de4c409c85e0da892346fff7f4a90d5c7034b7
Phuc <lephuc04b3@gmail.com>
2025-03-07 22:09:04 +0700

    feat: add BookRequest dto

---
+++ src/main/java/com/example/read_book_online/dto/request/BookRequest.java
@@ -0,0 +1,12 @@
+package com.example.read_book_online.dto.request;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class BookRequest {
+    private String title;
+    private Long authorId;
+    private Long categoryId;
+    private MultipartFile pdfFile;
+}

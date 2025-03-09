895cb74a8e1d9951a9b629b43c08a1fcf314d84d
Phuc <lephuc04b3@gmail.com>
2025-03-09 14:26:34 +0700

    refactor: change dto

--- src/main/java/com/example/read_book_online/dto/response/BookResponse.java
+++ src/main/java/com/example/read_book_online/dto/response/BookResponse.java
@@ -1,8 +1,12 @@
 package com.example.read_book_online.dto.response;
 
 import com.example.read_book_online.entity.Book;
+import com.example.read_book_online.entity.BookInteraction;
+import com.example.read_book_online.repository.BookRepository;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 @Builder
 @Data
@@ -17,19 +21,19 @@ public class BookResponse {
 
     private String pdfFilePath;
 
-    private Long views; // Tổng lượt xem
+    private Long totalViews;
 
-    private Long likes;
+    private Long totalLikes;
 
-    public static BookResponse from(Book book) {
+    public static BookResponse from(Book book, BookRepository bookRepository) {
         return BookResponse.builder()
                 .bookId(book.getBookId())
                 .categoryName(book.getCategory().getCategoryName())
                 .authorName(book.getAuthor().getAuthorName())
                 .title(book.getTitle())
                 .pdfFilePath(book.getPdfFilePath())
-                .views(book.getViews())
-                .likes(book.getLikes())
+                .totalLikes(bookRepository.countLikesByBookId(book.getBookId()))
+                .totalViews(bookRepository.sumViewsByBookId(book.getBookId()))
                 .build();
     }
 }

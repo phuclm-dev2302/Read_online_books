356530dbd7ad7cf59f40d0462bdd746a4e0e958b
Phuc <lephuc04b3@gmail.com>
2025-03-09 14:26:19 +0700

    refactor: add query countLikes and sumViews

--- src/main/java/com/example/read_book_online/repository/BookRepository.java
+++ src/main/java/com/example/read_book_online/repository/BookRepository.java
@@ -2,9 +2,19 @@ package com.example.read_book_online.repository;
 
 import com.example.read_book_online.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query("SELECT COUNT(bi) FROM BookInteraction bi " +
+            "WHERE bi.book.bookId = :bookId AND bi.liked = true")
+    Long countLikesByBookId(@Param("bookId") Long bookId);
+
+    @Query("SELECT SUM(bi.views) FROM BookInteraction bi" +
+            " WHERE bi.book.bookId = :bookId")
+    Long sumViewsByBookId(@Param("bookId") Long bookId);
 
 }

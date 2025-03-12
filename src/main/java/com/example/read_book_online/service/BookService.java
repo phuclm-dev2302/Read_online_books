79a2b682d716899b2a496fafa4e6af3d271bcade
Phuc <lephuc04b3@gmail.com>
2025-03-12 18:44:47 +0700

    refactor : add getBooks

--- src/main/java/com/example/read_book_online/service/BookService.java
+++ src/main/java/com/example/read_book_online/service/BookService.java
@@ -4,6 +4,7 @@ import com.example.read_book_online.dto.request.BookRequest;
 import com.example.read_book_online.dto.response.BookResponse;
 import com.example.read_book_online.dto.response.ResponseData;
 import org.springframework.core.io.Resource;
+import org.springframework.data.domain.Page;
 
 import java.io.FileNotFoundException;
 
@@ -11,5 +12,6 @@ public interface BookService {
     ResponseData<BookResponse> addBook(BookRequest bookRequest);
     ResponseData<BookResponse> getBookById(Long bookId);
     ResponseData<BookResponse> likeBook(Long bookId);
+    ResponseData<Page<BookResponse>> getBooks(int page, int size);
     Resource getBookPDF(Long bookId) throws FileNotFoundException;
 }

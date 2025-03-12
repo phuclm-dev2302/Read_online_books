ab407780462aed3450d459cb351d97a9b38d4121
Phuc <lephuc04b3@gmail.com>
2025-03-12 18:46:43 +0700

    feat: add RestTemplate

--- src/main/java/com/example/read_book_online/ReadBookOnlineApplication.java
+++ src/main/java/com/example/read_book_online/ReadBookOnlineApplication.java
@@ -2,6 +2,8 @@ package com.example.read_book_online;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ReadBookOnlineApplication {
@@ -9,5 +11,8 @@ public class ReadBookOnlineApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReadBookOnlineApplication.class, args);
     }
-
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }

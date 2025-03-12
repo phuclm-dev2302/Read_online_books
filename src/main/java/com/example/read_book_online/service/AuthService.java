de2ade9d7267c830de3ee59d705f6ee3a6427693
Phuc <lephuc04b3@gmail.com>
2025-03-12 18:45:50 +0700

    refactor : add method login with Google

--- src/main/java/com/example/read_book_online/service/AuthService.java
+++ src/main/java/com/example/read_book_online/service/AuthService.java
@@ -7,6 +7,7 @@ import com.example.read_book_online.dto.response.AuthResponse;
 import com.example.read_book_online.dto.response.ResponseData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 
 
 public interface AuthService {
@@ -14,4 +15,5 @@ public interface AuthService {
     ResponseData<String> register(SignUpRequest form);
     ResponseData<String> logout(HttpServletRequest request, HttpServletResponse response);
     ResponseData<String> confirmUser(long userId, String otpCode);
+    ResponseData<AuthResponse> loginWithGoogle(String accessToken);
 }

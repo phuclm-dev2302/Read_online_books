48691c1e09169ce3f7295cd0008a6679fc0a94b3
NguyenLam1708 <lam170804@gmail.com>
2025-03-26 01:10:52 +0700

    feat: add operation

--- src/main/java/com/example/read_book_online/controller/VipMembershipController.java
+++ src/main/java/com/example/read_book_online/controller/VipMembershipController.java
@@ -1,11 +1,9 @@
 package com.example.read_book_online.controller;
 
-import com.example.read_book_online.dto.request.RenewalRequest;
-import com.example.read_book_online.dto.request.VipMembershipRequest;
 import com.example.read_book_online.dto.response.ResponseData;
 import com.example.read_book_online.dto.response.VipMembershipResponse;
 import com.example.read_book_online.service.VipMembershipService;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -17,15 +15,16 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class VipMembershipController {
     private final VipMembershipService vipMembershipService;
-
+    @Operation(summary = "dang ki vip", description = "API cho nguoi dung dang ki vip theo so thang")
     @PostMapping
     public ResponseEntity<ResponseData<VipMembershipResponse>> registerVip(@RequestParam int time) {
         return ResponseEntity.ok(vipMembershipService.registerVip(time));
     }
-    @PutMapping
+    @PutMapping@Operation(summary = "API de gia han vip", description = "API cho phep nguoi dung gia han vip theo so thang")
     public ResponseEntity<ResponseData<VipMembershipResponse>> renewalVip(@RequestParam int time) {
         return ResponseEntity.ok(vipMembershipService.renewalVip(time));
     }
+    @Operation(summary = "API de nguoi dung xem tai khoan vip cua ho", description = "API de xem tai khoan Vip")
     @GetMapping
     public ResponseEntity<ResponseData<VipMembershipResponse>> getVip() {
         return ResponseEntity.ok(vipMembershipService.getVip());
--- src/main/java/com/example/read_book_online/service/VipMembershipService.java
+++ src/main/java/com/example/read_book_online/service/VipMembershipService.java
@@ -1,13 +1,9 @@
 package com.example.read_book_online.service;
 
-import com.example.read_book_online.dto.request.RenewalRequest;
-import com.example.read_book_online.dto.request.VipMembershipRequest;
 import com.example.read_book_online.dto.response.ResponseData;
 import com.example.read_book_online.dto.response.VipMembershipResponse;
 import com.example.read_book_online.entity.User;
 import com.example.read_book_online.entity.VipMembership;
-
-import java.time.LocalDate;
 
 public interface VipMembershipService {
     // tao vipm

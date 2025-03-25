e679b8d37f06b9ee52dccf5ebb53f2b03d31a157
NguyenLam1708 <lam170804@gmail.com>
2025-03-26 01:11:41 +0700

    delete: unnecessary files

--- src/main/java/com/example/read_book_online/config/exception/InvalidRenewalException.java
+++
@@ -1,7 +0,0 @@
-package com.example.read_book_online.config.exception;
-
-public class InvalidRenewalException extends RuntimeException {
-    public InvalidRenewalException(String message) {
-        super(message);
-    }
-}

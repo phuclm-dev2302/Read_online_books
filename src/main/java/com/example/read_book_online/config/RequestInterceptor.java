0a191508f03f4b340f6677914bb2d40407e664c3
Phuc <lephuc04b3@gmail.com>
2025-04-01 08:11:22 +0700

    feat: add interceptor

--- src/main/java/com/example/read_book_online/config/RequestInterceptor.java
+++ src/main/java/com/example/read_book_online/config/RequestInterceptor.java
@@ -6,46 +6,48 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 @Component
 public class RequestInterceptor implements HandlerInterceptor {
 
+    private static final String START_TIME = "startTime";
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-        //* Business logic just when the request is received and intercepted by this interceptor before reaching the controller
         try {
+            LocalDateTime startTime = LocalDateTime.now();
+            request.setAttribute(START_TIME, startTime);
             System.out.println("1 - preHandle() : Before sending request to the Controller");
             System.out.println("Method Type: " + request.getMethod());
             System.out.println("Request URL: " + request.getRequestURI());
-        }
-        //* If the Exception is caught, this method will return false
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
         return true;
     }
 
-    // Response is intercepted by this method before reaching the client
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        //* Business logic just before the response reaches the client and the request is served
         try {
             System.out.println("2 - postHandle() : After the Controller serves the request (before returning back response to the client)");
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    // This method is called after request & response HTTP communication is done.
+    // This method will be called after the request and response are completed
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        //* Business logic after request and response is Completed
         try {
-            System.out.println("3 - afterCompletion() : After the request and Response is completed");
-        }
-        catch (Exception e) {
+            LocalDateTime startTime = (LocalDateTime) request.getAttribute(START_TIME);
+            if (startTime != null) {
+                Duration duration = Duration.between(startTime, LocalDateTime.now());
+                System.out.println("3 - afterCompletion() : Time taken for request processing: " + duration.toMillis() + " ms");
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/main/java/com/example/read_book_online/config/RequestInterceptorConfig.java
+++ src/main/java/com/example/read_book_online/config/RequestInterceptorConfig.java
@@ -1,13 +1,15 @@
 package com.example.read_book_online.config;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
+@Component
 public class RequestInterceptorConfig implements WebMvcConfigurer {
+
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LocaleChangeInterceptor());
+        registry.addInterceptor(new RequestInterceptor());
     }
 }

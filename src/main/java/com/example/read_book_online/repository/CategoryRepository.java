f5f60ba253408f33ce31b969ea56b7490d235ebd
Lê Minh Phúc <134581737+phuclm-dev2302@users.noreply.github.com>
2025-04-29 09:43:21 +0700

    Update CategoryRepository.java
    Update CategoryRepository.java

--- src/main/java/com/example/read_book_online/repository/CategoryRepository.java
+++ src/main/java/com/example/read_book_online/repository/CategoryRepository.java
@@ -2,9 +2,10 @@ package com.example.read_book_online.repository;
 
 import com.example.read_book_online.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findByCategoryNameIn(List<String> categoryNames);
 }
